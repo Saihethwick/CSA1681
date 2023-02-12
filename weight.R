528938b59d00752b4380120d51fc45b36a1c7533
Saihethwick <112849639+Saihethwick@users.noreply.github.com>
2023-02-12 11:39:57 +0530

    Add files via upload

---
+++ weight.R
@@ -0,0 +1,9 @@
+input <- mtcars[,c('wt','mpg')]
+
+plot(x = input$wt,y = input$mpg,
+   xlab = "Weight",
+   ylab = "Milage",
+   xlim = c(2.5,5),
+   ylim = c(15,30),		 
+   main = "Weight vs Milage"
+)
